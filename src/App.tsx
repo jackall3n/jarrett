b2099f333b88aaf4f3c768ea8959c92f1ed78282
Jack Allen <jackallensemail+git@gmail.com>
2019-08-14 19:36:52 +0100

    all

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 const App: React.FC = () => {
+
+  const [showAnswer, setShowAnswer] = useState(false);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <br />
+      <h1>Is this guy a cunt?</h1>
+      <br />
+
+      <div
+        className="face"
+      />
+      
+      <br />
+      <button className="button" onClick={() => setShowAnswer(true)}>Click here to find out</button>
+
+      {showAnswer && (
+        <h2>yes.</h2>
+      )}
     </div>
   );
 }
